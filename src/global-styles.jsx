1ed5951c7613363237f4cec29e3c08964da22db4
André Dias <andredias.us@gmail.com>
2020-11-04 11:46:22 -0300

    Iniciando footer

---
+++ src/global-styles.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif, Arial, Helvetica, sans-serif, sans-serif;
+        background-color: #000;
+        color: #333;
+        font-size: 16px;
+    }
+`;
+
+export default GlobalStyles;
